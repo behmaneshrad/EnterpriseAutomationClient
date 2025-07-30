17eb8649f4fa290874201ebe3b73e7f1f43147da
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-07-30 23:24:04 +0330

    Initial commit of new frontend project with updated line endings

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100 text-blue-900 p-4 text-center" >
+      <h1 className="text-5xl font-extrabold text-blue-700 mb-6">به سیستم اتوماسیون سازمانی خوش آمدید!</h1>
+      <p className="text-xl max-w-xl">اینجا مرکز کنترل فرآیندهای کسب‌وکار است.</p>
+    </div>
+  )
+}
+
+export default page;
