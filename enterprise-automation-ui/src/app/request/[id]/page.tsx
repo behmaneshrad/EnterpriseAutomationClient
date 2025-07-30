a601be7eb7f4e9c013018fa8d49127974ddffdc9
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-07-31 01:21:08 +0330

    updated request

--- enterprise-automation-ui/src/app/request/[id]/page.tsx
+++ enterprise-automation-ui/src/app/request/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const RequestDetailPage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const page = () => {
   )
 }
 
-export default page
+export default RequestDetailPage
--- enterprise-automation-ui/src/app/request/new/page.tsx
+++ enterprise-automation-ui/src/app/request/new/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NewPage = () => {
+const NewRequestPage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const NewPage = () => {
   )
 }
 
-export default NewPage
+export default NewRequestPage;
