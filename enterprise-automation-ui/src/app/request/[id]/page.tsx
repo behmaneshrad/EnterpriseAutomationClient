28408854280050371136cda47d5d3a70cbed7788
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-01 19:48:05 +0330

    update requestid

--- enterprise-automation-ui/src/app/request/[id]/page.tsx
+++ enterprise-automation-ui/src/app/request/[id]/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client";
 
-const RequestDetailPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React from "react";
+
+interface RequestDetailPageProps {
+  params: {
+    id: string;
+  };
 }
 
-export default RequestDetailPage
+const RequestDetailPage: React.FC<RequestDetailPageProps> = ({ params }) => {
+  const { id } = params;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        جزئیات درخواست <span className="text-blue-600">{id}</span>
+      </h1>
+    </div>
+  );
+};
+
+export default RequestDetailPage;
