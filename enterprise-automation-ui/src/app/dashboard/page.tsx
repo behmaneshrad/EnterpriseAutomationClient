1ae0b4a810f6faaa473ee7672d0672d6bd628792
faezeVahdat <faeze.vh@gmail.com>
2025-08-01 22:40:04 +0330

    edited dashboardpage

--- enterprise-automation-ui/src/app/dashboard/page.tsx
+++ enterprise-automation-ui/src/app/dashboard/page.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-const DashboardPage = () => {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">داشبورد</h1>
-      <p className="text-lg text-gray-700">این صفحه داشبورد است.</p>
+    <div className="p-8 flex justify-between mx-8 mt-4">
+      
+      <p className="mt-2 text-blue-400"> {session.user?.name || "فائزه وحدت"} </p>
+      <h1 className="text-xl text-blue-400 font-bold">داشبورد</h1>
     </div>
   );
-};
-
-export default DashboardPage;
+}
