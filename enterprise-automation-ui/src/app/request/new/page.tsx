173497e959be241d18f36f34236ccac02111ac11
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-01 19:48:45 +0330

    updated requestnew

--- enterprise-automation-ui/src/app/request/new/page.tsx
+++ enterprise-automation-ui/src/app/request/new/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const NewRequestPage = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        ثبت درخواست جدید
+      </h1>
+      <p className="text-lg text-gray-700">فرم درخواست اینجاست.</p>
     </div>
-  )
-}
+  );
+};
 
 export default NewRequestPage;
