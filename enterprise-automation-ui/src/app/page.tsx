402f79efa05e4b5f37dfa6e94418f8ff9490d0d3
faezeVahdat <faeze.vh@gmail.com>
2025-08-01 22:41:14 +0330

    edited page

--- enterprise-automation-ui/src/app/page.tsx
+++ enterprise-automation-ui/src/app/page.tsx
@@ -1,14 +1,20 @@
-"use client";
 
-import React from 'react';
+'use client';
 
-const page = () => {
+import { useRouter } from "next/navigation";
+
+export default function HomePage() {
+  const router = useRouter();
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100 text-blue-900 p-4 text-center" >
-      <h1 className="text-5xl font-extrabold text-blue-700 mb-6">به سیستم اتوماسیون سازمانی خوش آمدید!</h1>
-      <p className="text-xl max-w-xl">اینجا مرکز کنترل فرآیندهای کسب‌وکار است.</p>
-    </div>
-  )
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl text-blue-950 font-bold mb-6">خوش آمدید </h1>
+      <button
+        onClick={() => router.push("/login")}
+        className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition-all"
+      >
+        ورود به حساب
+      </button>
+    </main>
+  );
 }
-
-export default page;
