16d1465f21478eef9b31cc86dc20e32f6f3627aa
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-05 22:11:12 +0330

    created useAuth.ts

---
+++ enterprise-automation-ui/src/hooks/useAuth.ts
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import { useAtom } from "jotai";
+import { sessionAtom, accessTokenAtom, authStatusAtom } from "@/state/auth";
+import { useSession } from "next-auth/react";
+
+const useAuth = () => {
+    const {data: session, status} = useSession();
+
+    const [authSession, setAuthSession] = useAtom(sessionAtom);
+    const [authStatus, setAuthStatus] = useAtom(authStatusAtom);
+    const [accessToken] = useAtom(accessTokenAtom);
+
+    useEffect(() => {
+        setAuthSession(session);
+        setAuthStatus(status);
+    }, [session, status, setAuthSession, setAuthStatus]);
+  
+  
+    return {
+        session: authSession,
+        status: authStatus,
+        accessToken,
+    };
+}
+
+export default useAuth
+
