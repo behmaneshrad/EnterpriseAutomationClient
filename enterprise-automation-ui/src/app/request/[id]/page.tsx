4a37280094bfe0057f7a7c9299ebeaebd4738f16
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-05 12:16:41 +0330

    updated requestpage

--- enterprise-automation-ui/src/app/request/[id]/page.tsx
+++ enterprise-automation-ui/src/app/request/[id]/page.tsx
@@ -1,22 +1,11 @@
-"use client";
+import React from 'react'
 
-import React from "react";
-
-interface RequestDetailPageProps {
-  params: {
-    id: string;
-  };
-}
-
-const RequestDetailPage: React.FC<RequestDetailPageProps> = ({ params }) => {
-  const { id } = params;
+const RequestDetailPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        جزئیات درخواست <span className="text-blue-600">{id}</span>
-      </h1>
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
 export default RequestDetailPage;
