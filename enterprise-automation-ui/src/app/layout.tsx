772448396b56e855378419538a9ee4b1078d30ae
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-05 12:25:49 +0330

    updated layout.tsx

--- enterprise-automation-ui/src/app/layout.tsx
+++ enterprise-automation-ui/src/app/layout.tsx
@@ -1,16 +1,23 @@
+import { Inter } from "next/font/google";
 import "./globals.css";
+import React from 'react';
 
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Enterprise Automation UI",
-  description: "enterprise automation hub.",
-},
+  description: "Your enterprise automation hub.",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="fa">
       <body>
-        {children}
+          {children}
       </body>
     </html>
   );
