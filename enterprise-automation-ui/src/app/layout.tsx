54aba58394a4afc80ba74e076342e06a7f604b1f
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-05 23:22:17 +0330

    updated Layout

--- enterprise-automation-ui/src/app/layout.tsx
+++ enterprise-automation-ui/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import React from "react";
 import { Provider } from "jotai";
 import { SessionProvider } from "next-auth/react";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <body>
+        <Header/>
         <SessionProvider>
           <Provider>{children}</Provider>
         </SessionProvider>
