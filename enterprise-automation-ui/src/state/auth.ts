820c148a49c48ac16490593c89901b2b03a504c8
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-06 20:39:18 +0330

    updated auth.ts

---
+++ enterprise-automation-ui/src/state/auth.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { atom } from 'jotai';
+import { Session } from 'next-auth';
+
+// Atom for storing session information
+export const sessionAtom = atom<Session | null>(null);
+ 
+// Atom for storing accessToken
+export const accessTokenAtom = atom<string | undefined>((get) => {
+    const session = get(sessionAtom);
+    return session?.accessToken;
+});
+
+
+// Atom for maintaining authentication status
+export const authStatusAtom = atom<'Loading' | 'authenticated' | 'unauthenticated'>('Loading');
