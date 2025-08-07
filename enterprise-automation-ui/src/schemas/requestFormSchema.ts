0d40224174005b29d086b23ea4e28d3e4ff7c2a0
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-07 22:39:16 +0330

    created requestFormSchema

---
+++ enterprise-automation-ui/src/schemas/requestFormSchema.ts
@@ -0,0 +1,14 @@
+import {z} from "zod";
+
+export const requestFormSchema = z.object({
+    title: z
+    .string()
+    .min(5,{message: 'عنوان حداقل باید ۵ کاراکتر باشد.'})
+    .max(100, {message: 'عنوان نمی‌تواند بیشتر از ۱۰۰ کاراکتر باشد.'}),
+    description: z
+    .string()
+    .min(20, {message: 'شرح باید حداقل ۲۰ کاراکتر داشته باشد.'})
+    .max(500, {message: 'شرح نمی‌تواند بیشتر از ۵۰۰ کاراکتر باشد.'}),
+});
+
+export type RequestFormSchema = z.infer<typeof requestFormSchema>;
