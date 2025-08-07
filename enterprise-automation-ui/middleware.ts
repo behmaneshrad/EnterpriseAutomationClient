632f5563ce56a763b80674f587b235575da56db5
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-07 12:46:51 +0330

    updated middleware

--- enterprise-automation-ui/middleware.ts
+++ enterprise-automation-ui/middleware.ts
@@ -1,11 +1,23 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  pages: {
-    signIn: "/login",
+export default withAuth(
+  function middleware(request) {
+    const isLoadingPage = request.nextUrl.pathname.startsWith("/login");
+    const isAuthenticated = !!request.nextauth.token;
+    // اگر کاربر وارد شده باشد و در صفحه لاگین باشد او را به صفحه اصلی هدایت میکند
+    if (isLoadingPage && isAuthenticated) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    // اگر کاربر لاگین نکرده باشد به صورت خودکار به صفحه لاگین میرود 
   },
-});
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/profile/:path*", "/login/:path*"],
 };
