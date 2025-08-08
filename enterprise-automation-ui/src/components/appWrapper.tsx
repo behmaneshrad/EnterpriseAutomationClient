70ac92a31ed99eabebf4a10b90e7d1aeb8b35ef0
faezeVahdat <faeze.vh@gmail.com>
2025-08-09 02:27:17 +0330

    edited

--- enterprise-automation-ui/src/components/appWrapper.tsx
+++ enterprise-automation-ui/src/components/appWrapper.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { RoleProvider } from "@/context/RoleContext";
+import RoleLoader from "./RoleLoader";
 import Header from "./Header";
 import React, { ReactNode } from "react";
+
 
 const AppWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <SessionProvider>
+      <RoleProvider>
         <Header />
         <main className="p-4">{children}</main>
+        </RoleProvider>
     </SessionProvider>
   );
 };
