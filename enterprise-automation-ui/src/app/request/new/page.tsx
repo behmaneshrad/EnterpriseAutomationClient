ec2d8a3f26cb886027b1eaba0f745d1a3984938f
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-10 13:46:17 +0330

    updated request/new

--- enterprise-automation-ui/src/app/request/new/page.tsx
+++ enterprise-automation-ui/src/app/request/new/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import RequestForm from "@/components/RequestForm";
 
 const NewRequestPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        ثبت درخواست جدید
+        ارسال درخواست جدید
       </h1>
-      <p className="text-lg text-gray-700">فرم درخواست اینجاست.</p>
+      <RequestForm/>
     </div>
   );
 };
