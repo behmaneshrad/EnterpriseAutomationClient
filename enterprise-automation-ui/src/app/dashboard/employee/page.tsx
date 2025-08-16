f8baf94cf096db2c45cdcd301467ca2846f774a9
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-16 11:06:30 +0330

    updated dashboard.employee

--- enterprise-automation-ui/src/app/dashboard/employee/page.tsx
+++ enterprise-automation-ui/src/app/dashboard/employee/page.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-const EmployeeDashboardPage = () => {
+const EmployeeDashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  // بررسی وضعیت ورود و نقش کاربر
+  if (!session || user?.roles?.[0] !== "employee") {
+    // اگر کاربر لاگین نکرده باشد یا با نقش کارمند وارد نشده باشد به صفحه اصلی یا لاگین منتقل میشود.
+    redirect("/login");
+  }
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold mb-4">داشبورد کارمندان</h1>
       <p className="text-gray-600 dark:text-gray-300">
-        محتوای داشبورد کارمندان
+        به داشبورد کارمندان خوش آمدید، {user?.name}.
       </p>
     </div>
   );
