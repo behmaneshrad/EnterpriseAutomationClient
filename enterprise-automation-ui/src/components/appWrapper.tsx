bb1521ba1d65bf9be658b004da4260422d4862c0
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-16 10:46:51 +0330

    updated appWrpper

--- enterprise-automation-ui/src/components/appWrapper.tsx
+++ enterprise-automation-ui/src/components/appWrapper.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+
 import { RoleProvider } from "@/context/RoleContext";
-import RoleLoader from "./RoleLoader";
 import Header from "./Header";
 import ToastProvider from "./ToastProvider";
 import React, { ReactNode } from "react";
@@ -10,13 +9,11 @@ import React, { ReactNode } from "react";
 
 const AppWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <SessionProvider>
       <RoleProvider>
         <Header />
         <main className="p-4">{children}</main>
         <ToastProvider/>
         </RoleProvider>
-    </SessionProvider>
   );
 };
 
