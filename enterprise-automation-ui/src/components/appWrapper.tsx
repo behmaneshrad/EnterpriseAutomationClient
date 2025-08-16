bea676e1746d17cdf77358a6a25d847355fad360
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-16 20:29:01 +0330

    updated appWrapper

--- enterprise-automation-ui/src/components/appWrapper.tsx
+++ enterprise-automation-ui/src/components/appWrapper.tsx
@@ -4,16 +4,19 @@
 import { RoleProvider } from "@/context/RoleContext";
 import Header from "./Header";
 import ToastProvider from "./ToastProvider";
+import { AuthProvider } from "@/context/AuthContext";
 import React, { ReactNode } from "react";
 
 
 const AppWrapper = ({ children }: { children: ReactNode }) => {
   return (
+    <AuthProvider>
       <RoleProvider>
         <Header />
         <main className="p-4">{children}</main>
         <ToastProvider/>
         </RoleProvider>
+        </AuthProvider>
   );
 };
 
