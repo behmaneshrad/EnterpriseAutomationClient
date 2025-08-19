569725597dbfa8519617bd09eaf6889c08f4f88d
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-19 12:05:01 +0330

    updated loginPage

--- enterprise-automation-ui/src/app/login/page.tsx
+++ enterprise-automation-ui/src/app/login/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import React from "react";
+import React, { useEffect } from "react";
 
 const LoginPage = () => {
+  useEffect(() => {
+    signIn('keycloak', {callbackUrl: '/'})
+  }, [])
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
       <h1 className="text-3xl font-bold mb-6 text-gray-800 dark:text-gray-100">ورود به اتوماسیون اداری</h1>
