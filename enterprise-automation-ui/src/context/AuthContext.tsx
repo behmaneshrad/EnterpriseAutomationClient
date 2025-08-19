036073cc00db078d331223ebdc5f7f4b41e6766b
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-19 09:59:10 +0330

    updated AuthContext.tsx

--- enterprise-automation-ui/src/context/AuthContext.tsx
+++ enterprise-automation-ui/src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { signOut, useSession } from "next-auth/react";
 import React, {
   createContext,
   useContext,
@@ -8,15 +9,16 @@ import React, {
   ReactNode,
 } from "react";
 
-//اینترفیس برای تعریف ساختار داده های کاربر
+// تایپ داده های کاربر 
 interface User {
   id?: string;
   name?: string | null;
   email?: string | null;
   role?: string;
+  roles?: string;
 }
 
-// اینترفیس برای تعریف ساختار داده های توکن
+// تایپ داده های توکن
 interface AuthTokens {
   accessToken?: string;
   refreshToken?: string;
@@ -28,57 +30,57 @@ interface AuthContextType {
   user: User | null;
   tokens: AuthTokens | null;
   isAuthenticated: boolean;
-  login: (user: User, tokens: AuthTokens) => void;
   logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [user, setUser] = useState<User | null>(null);
-  const [tokens, setTokens] = useState<AuthTokens | null>(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const {data: session, status} = useSession();
+  const [initialUserData, setInitialUserData] = useState<User | null>(null);
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const storedTokens = localStorage.getItem("tokens");
-
-    if (storedUser && storedTokens) {
-      setUser(JSON.parse(storedUser));
-      setTokens(JSON.parse(storedTokens));
-      setIsAuthenticated(true);
+    if (status === 'authenticated' && session?.user) {
+      // فقط در اولین لاگین اطلاعات ذخیره شود
+      if (!localStorage.getItem('user')) {
+      const userData = {
+        name: session.user.name,
+        email: session.user.email,
+      };
+      localStorage.setItem('user', JSON.stringify(userData));
     }
-  }, []);
-  //   تابع  login برای ذخیره اطلاعات و بروزرسانی state
-  const login = (userData: User, authTokens: AuthTokens) => {
-    localStorage.setItem("user", JSON.stringify(userData));
-    localStorage.setItem("tokens", JSON.stringify(authTokens));
-    setUser(userData);
-    setTokens(authTokens);
-    setIsAuthenticated(true);
-  };
+    setInitialUserData(session.user as User);
+    } else if (status === 'unauthenticated') {
+      //را پاک میکنیم localstorage کرده  Logout  اگر کاربر
+      localStorage.removeItem('user');
+      setInitialUserData(null);
+    }
+}, [status, session]);
 
   const logout = () => {
-    localStorage.removeItem("user");
-    localStorage.removeItem("tokens");
-    setUser(null);
-    setTokens(null);
-    setIsAuthenticated(false);
+    signOut({callbackUrl: '/login'});
+  }
+
+  const tokens = {
+    accessToken: session?.accessToken,
+    refreshToken: session?.refreshToken,
+    idToken: session?.idToken,
   };
 
+  const user = initialUserData || session?.user || null;
+  const isAuthenticated = status === 'authenticated';
+
   return (
-    <AuthContext.Provider
-      value={{ user, tokens, isAuthenticated, login, logout }}
-    >
+    <AuthContext.Provider value={{user, tokens, isAuthenticated, logout}}>
       {children}
     </AuthContext.Provider>
-  );
+  )
 }
 
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error("useAuth must be used within an AuthProvider");
+    throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
 }
