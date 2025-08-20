42dedd6231d8b048721f553af609c455d4dfa2b0
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-20 23:00:01 +0330

    updated ToastProvider

--- enterprise-automation-ui/src/components/ToastProvider.tsx
+++ enterprise-automation-ui/src/components/ToastProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Toaster } from "react-hot-toast";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const ToastProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
