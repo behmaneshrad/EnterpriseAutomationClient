b1005aa5c1631f779bf07be48568cec22878195f
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-08-25 18:56:25 +0330

    Updated header

--- enterprise-automation-ui/src/components/Header.tsx
+++ enterprise-automation-ui/src/components/Header.tsx
@@ -3,8 +3,24 @@
 import Link from "next/link";
 import { useAuth } from "@/context/AuthContext";
 
+
+// تابع کمکی برای ترجمه نقش ها
+const getPersianRole = (role: string | undefined) => {
+    switch (role) {
+    case 'admin':
+      return 'مدیر';
+    case 'approver':
+      return 'تاییدکننده';
+    case 'employee':
+      return 'کارمند';
+    default:
+      return 'کاربر';
+  }
+}
+
 const Header = () => {
   const { user, isAuthenticated, logout } = useAuth();
+  const userRole = user?.roles?.[0];
 
   return (
     <header className="bg-gray-50 text-gray-900 p-4 flex justify-between items-center">
@@ -16,22 +32,18 @@ const Header = () => {
       </Link>
 
       <nav>
-        {!isAuthenticated && !user ? (
-          <div>در حال بارگذاری...</div>
-        ) : isAuthenticated ? (
-          // نمایش اطلاعات و لینک ها برای کاربران لاگین کرده
+        {isAuthenticated && user ? (
           <div className="flex items-center space-x-4 rtl:space-x-reverse ">
-            {/* نام، ایمیل و نقش کاربر */}
+            {/* نمایش نقش کاربر */}
             <span className="text-sm">
-              {user?.name} | {user?.email} | ({user?.roles?.[0]})
+              نقش: {getPersianRole(userRole)}
             </span>
 
-            {/* لینک پروفایل */}
+            {/* پروفایل من */}
             <Link href="/profile" className="hover:text-blue-200">
               پروفایل من
             </Link>
 
-            {/* خروج */}
             <button
               onClick={logout}
               className="bg-red-500 hover:bg-red-700 text-white py-1 px-3 rounded-lg transition-colors duration-200 mx-8"
@@ -40,7 +52,7 @@ const Header = () => {
             </button>
           </div>
         ) : (
-          // نمایش لینک ورود برای کاربران مهمان
+          
           <Link
             href="/login"
             className="bg-blue-800 hover:bg-blue-900 text-white py-2 px-4 rounded-lg transition-colors duration-200 "
