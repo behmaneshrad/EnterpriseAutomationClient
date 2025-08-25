d808506438479ca0c23e0bb6f81a5589c3393f30
faezeVahdat <faeze.vh@gmail.com>
2025-08-25 08:05:22 +0330

    created

---
+++ enterprise-automation-ui/src/services/workflows.ts
@@ -0,0 +1,22 @@
+export async function getWorkflowById(id: number) {
+    try {
+      const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
+      const response = await fetch(`${BASE_URL}/api/WorkflowDefinitions/GetWorkflowDefinitionsAndStepById/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`خطا در دریافت اطلاعات: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error: any) {
+      console.error('خطا در دریافت Workflow:', error.message);
+      throw error;
+    }
+  }
