6524295e916ddb98a8c2c40b76f734532cea746c
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-09-02 09:32:44 +0330

    updated page.tsx

--- enterprise-automation-ui/src/app/dashboard/employee/page.tsx
+++ enterprise-automation-ui/src/app/dashboard/employee/page.tsx
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
 
 const EmployeeDashboardPage = async () => {
   const session = await getServerSession(authOptions);
