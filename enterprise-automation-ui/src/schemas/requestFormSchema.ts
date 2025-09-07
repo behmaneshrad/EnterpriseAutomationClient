1f8620aa1401a2edcf60bda2e8f98f31020a0f7b
Shakila Mehri <shakila.mehri.98@gmail.com>
2025-09-07 20:17:19 +0330

    #"1404/06/16" requestFormSchema: add validation for startDate, endDate and file uploaded

--- enterprise-automation-ui/src/schemas/requestFormSchema.ts
+++ enterprise-automation-ui/src/schemas/requestFormSchema.ts
@@ -1,5 +1,12 @@
 import * as z from "zod";
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 مگ
+const  ACCEDPTED_FILE_TYPES = [
+    "application/pdf",
+    "image/jpeg",
+    "image/png",
+];
+
 export const requestFormSchema = z.object({
     title: z
     .string()
@@ -9,7 +16,32 @@ export const requestFormSchema = z.object({
     .string()
     .min(20, {message: 'شرح باید حداقل ۲۰ کاراکتر داشته باشد.'})
     .max(500, {message: 'شرح نمی‌تواند بیشتر از ۵۰۰ کاراکتر باشد.'}),
-    workflowId: z.string().min(1, "انتخاب فرایند الزامی است")
-});
+    workflowId: z.string().min(1, "انتخاب فرایند الزامی است"),
+
+    startDate: z.string().nonempty("تاریخ شروع الزامی است"),
+    endDate: z.string().nonempty("تاریخ پایان الزامی است"),
+    file: z
+    .instanceof(File)
+    .optional()
+    .refine((file) => !file || file.size <= MAX_FILE_SIZE, {
+        message: "حجم فایل نباید بیشتر از 5 مگابایت باشد.",
+    })
+    .refine((file) => !file || ACCEDPTED_FILE_TYPES.includes(file.type), {
+        message: "باشد pdf یا  (jpg, png) فرمت فایل باید "
+    })
+})
+.refine(
+    (data) => {
+        if (!data.startDate || !data.endDate) return true;
+        const start = new Date(data.startDate);
+        const end = new Date(data.endDate);
+        return start <= end;
+    },
+    {
+        message: "تاریخ پایان باید بعد از تاریخ شروع باشد.",
+        path: ["endDate"],
+    }
+);
+
 
 export type RequestFormSchema = z.infer<typeof requestFormSchema>;
